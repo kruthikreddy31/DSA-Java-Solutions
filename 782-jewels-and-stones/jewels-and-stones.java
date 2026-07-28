5de0ac1f7304a11b6042c999a1cc25ccd0dc6379
Elmette Kruthik Reddy <kruthik0131@gmail.com>
2026-07-28 09:30:35 +0530

    Time: 1 ms (63.39%) | Memory: 43.4 MB (8.95%) - LeetSync

---
+++ 782-jewels-and-stones/jewels-and-stones.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(int i=0;i<jewels.length();i++){
+            map.put(jewels.charAt(i),0);
+        }
+        for(int i=0;i<stones.length();i++){
+            if(map.containsKey(stones.charAt(i))){
+                map.put(stones.charAt(i),map.getOrDefault(stones.charAt(i),0)+1);
+            }
+        }
+        int count=0;
+        for(int i=0;i<jewels.length();i++){
+                count+=map.get(jewels.charAt(i));
+
+        }
+        return count;
+    }
+}
